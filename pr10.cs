6a07673172ba3256b0de3058855572527790e17e
ElizaBeth <167125218+LikaLi1@users.noreply.github.com>
2025-10-22 14:43:52 +0300

    pr10.cs

---
+++ pr10.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            Int32.TryParse(Console.ReadLine(), out int A);
+
+            long fact = 1;
+            double sum = 0;
+
+            for (int i = 1; i <= A; i++) 
+            {
+                fact *= i;
+                sum += fact;
+
+            }
+            Console.WriteLine(fact);
+        }
+    }
+}
