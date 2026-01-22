951c3f3755388268232d145cb67e867e3aa93daa
ElizaBeth <167125218+LikaLi1@users.noreply.github.com>
2026-01-22 10:28:08 +0300

    pr52.cs

---
+++ pr52.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите слово: ");
+            string word = Console.ReadLine();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                Console.WriteLine(word[i]);
+            }
+        }
+    }
+}
