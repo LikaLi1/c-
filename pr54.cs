e7e5b9d958091d6e608102dfe2d89e01adcc9103
ElizaBeth <167125218+LikaLi1@users.noreply.github.com>
2026-01-23 20:58:51 +0300

    pr54.cs

---
+++ pr54.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace ConsoleApp20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int[,] a = new int[5, 7];
+
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    a[i, j] = rnd.Next(0, 100);
+                }
+            }
+
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    Console.Write($"{a[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            int max = a[0, 0];
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    if (a[i, j] > max)
+                    {
+                        max = a[i, j];
+                    }
+                }
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
