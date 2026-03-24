cb71fd5ef7b2ffefd81b96b895f515302d0b9055
ElizaBeth <167125218+LikaLi1@users.noreply.github.com>
2026-03-24 08:59:41 +0300

    pr69.cs

--- pr69.cs
+++ pr69.cs
@@ -3,31 +3,28 @@ using System;
 
 namespace Pay
 {
-    interface implement
+    interface IPay
     {
         decimal? money { get; set; }
-        void conclusion()
-        {
-            Console.WriteLine($"Вывод денежных средств в размере: {money}");
-        }
+        void conclusion();
     }
-    class CreditCardPayment : implement
+    class CreditCardPayment : IPay
     {
         public decimal? money { get; set; }
-        public void conclusion() 
+        public void conclusion()
         {
             Console.WriteLine($"Вывод кредитных денежных средств в размере: {money}");
         }
     }
-    class PayPalPayment : implement
+    class PayPalPayment : IPay
     {
         public decimal? money { get; set; }
-        public void conclusion() 
+        public void conclusion()
         {
             Console.WriteLine($"Вывод электронных денежных средств в размере: {money}");
         }
     }
-    class CryptoPayment : implement
+    class CryptoPayment : IPay
     {
         public decimal? money { get; set; }
         public void conclusion()
@@ -35,15 +32,36 @@ namespace Pay
             Console.WriteLine($"Вывод ненастоящих денежных средств в размере: {money}");
         }
     }
+    class PaymentProcessor
+    {
+        private IPay _pay;
+        public PaymentProcessor(IPay pay)
+        {
+            _pay = pay;
+        }
+        public void ProcessorPayment(decimal money)
+        {
+            _pay.money = money;
+            _pay.conclusion();
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
             CreditCardPayment credit = new CreditCardPayment();
-            credit.money = 2000;
-            credit.conclusion();
+            PayPalPayment paypal = new PayPalPayment();
+            CryptoPayment crypto = new CryptoPayment();
 
-            implement m = new CreditCardPayment();
+            var process = new PaymentProcessor(credit);
+            process.ProcessorPayment(500);
+
+            var processs = new PaymentProcessor(paypal);
+            process.ProcessorPayment(10000);
+
+            var processss = new PaymentProcessor(crypto);
+            process.ProcessorPayment(10);
         }
     }
 }
