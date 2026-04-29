467c74097c660a800ec69f6b01f2fc8bd8494f95
ElizaBeth <167125218+LikaLi1@users.noreply.github.com>
2026-04-29 09:20:45 +0300

    pr81.cs

--- pr81.cs
+++ pr81.cs
@@ -3,23 +3,36 @@ Dictionary<string, string> users = new Dictionary<string, string>(16) //ёмко
 {
     ["id_1"] = "user_1",
     ["id_2"] = "user_2",
-    ["id_3"] = "user_3",
+    ["id_3"] = "user_3"
 };
+
+Dictionary<string, string> d = new Dictionary<string, string>() //ёмкость словаря
+{
+    ["id_4"] = "user_1",
+    ["id_5"] = "user_2",
+    ["id_6"] = "user_3"
+};
+
+users.TryAdd("id_1", "hello");
+Console.WriteLine(users.TryGetValue(users["id_7"], out string? text));
 
 KeyValuePair<string, string> a = new KeyValuePair<string, string>("id_1", "user_1"); //плучаем значение
 
 Console.WriteLine(users["id_1"]);
 
+Console.WriteLine();
 foreach (var user in users)
 {
     Console.WriteLine(user);
 }
 
+Console.WriteLine();
 foreach (var user in users)
 {
     Console.WriteLine($"{user.Key} - {user.Value} ");
 }
 
+Console.WriteLine();
 foreach (var (key, val) in users)
 {
     Console.WriteLine($"{key} - {val} ");
