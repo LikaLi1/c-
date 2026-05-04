b2535bbad011ff00a78acdf0ffbf5c5b1fdf674f
ElizaBeth <167125218+LikaLi1@users.noreply.github.com>
2026-05-04 10:41:27 +0300

    pr83.cs

---
+++ pr83.cs
@@ -0,0 +1,50 @@
+using ConsoleApp17;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    internal class Student
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public Student(string? name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+    class Program
+    {
+        static void main()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>()
+            {
+                new Student("Vadim", 18),
+                new Student("Maxim", 18)
+            };
+            students.CollectionChanged += StudentChanged;
+
+            students.Add(new Student("Alex", 18));
+            students.Add(new Student("Alex", 18));
+            students.Add(new Student("Alex", 18));
+
+            void StudentChanged(object? sender, NotifyCollectionChangedEventArgs e)
+            {
+                if (e.NewItems?[0] is Student newStudent)
+                {
+                    Console.WriteLine("Add");
+                    foreach (var student in e.NewItems)
+                    {
+                        Console.WriteLine(student);
+                    }
+                }
+            }
+        }
+    }
+}
