a79f561fefb3da10683a748a722ebb0ef5550415
ElizaBeth <167125218+LikaLi1@users.noreply.github.com>
2026-05-18 11:03:31 +0300

    pr87.cs

---
+++ pr87.cs
@@ -0,0 +1,89 @@
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "text.txt";
+            string text = "Hello World";
+
+            string fileText = File.ReadAllText(path);
+            //сохранил в архиве этот созданный документ
+            File.WriteAllText(path, text);
+            File.WriteAllLines();
+            File.WriteAllBytes();
+
+            File.ReadAllText(path, text);
+            File.ReadAllLines();
+            File.ReadAllBytes();
+
+            File.AppendAllText(path, text);
+            File.AppendAllLines();
+            File.AppendAllBytes();
+
+            Console.WriteLine(fileText);
+
+            .CteateText();
+            .Append();
+            .OpenText();
+
+
+
+            string path2 = @"C:\";
+            string path3 = null;
+            try
+            {
+                Console.WriteLine(File.ReadAllText(path2));
+                Console.WriteLine(path3.Length);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+                return;
+            }
+            finally
+            {
+                Console.WriteLine("Всегда выполяется после работы try/catch игнорируя return");
+            }
+
+
+
+            FileInfo file = new FileInfo(@"text.txt");
+
+            FileStream;
+            StreamWriter;
+            StreamReader;
+            StreamContent;
+
+
+
+            string text2 = "Hello World";
+            FileInfo files = new FileInfo(@"text.txt");
+            //StreamWriter? writer = null;
+            //try
+            //{
+            //    writer = files.CreateText();
+            //    writer.WriteLine(text2);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //finally
+            //{
+            //    writer.Close();
+            //}
+            //вместо try/catch/finally используем using
+            using (StreamWriter writer = file.CreateText())
+            {
+                writer.WriteLine(text2);
+            }
+            using (StreamWriter reader = file.OpenText())
+            {
+                string? line;
+                while((line = reader.ReadLine()) != null)
+                    Console.WriteLine(line);
+            }
+        }
+    }
+}
